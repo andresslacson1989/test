b417831beb17ce890f31739402e10982893d1531
andresslacson1989 <48279587+andresslacson1989@users.noreply.github.com>
2023-01-24 08:07:47 +0800

    Update telegram.sh

--- telegram.sh
+++ telegram.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-
+/bin/cat <<"BytesPH3" >/root/telegram.sh
+    #!/bin/bash
     GROUP_ID='5075831208'
     BOT_TOKEN='5405146881:AAEWI1C917dE_K8JzKwR4F_v2s1s8jX-8aw'
 
     # this 3 checks (if) are not necessary but should be convenient
-
-    if [ "$1" == "-h" ]; then
-      echo "Usage: `basename $0` \"text message\""
-      exit 0
-    fi
-
-    if [ -z "$1" ]
-          then
-            echo "Add message text as second arguments"
-          exit 0
-    fi
 
     if [ "$#" -ne 1 ]; then
         echo "You can pass only one argument. For string with spaces put it on quotes"
@@ -22,6 +12,8 @@
     fi
 
     curl -s --data "text=$1" --data "chat_id=$GROUP_ID" 'https://api.telegram.org/bot'$BOT_TOKEN'/sendMessage' > /dev/null
+
+BytesPH3
 
     chmod +x /root/telegram.sh
     cd
