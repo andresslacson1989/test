e5ba4cc3c017e9d4af4cedd7d721e3663fb24502
andresslacson1989 <48279587+andresslacson1989@users.noreply.github.com>
2023-01-24 08:05:54 +0800

    telegram.sh

---
+++ telegram.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+    GROUP_ID='5075831208'
+    BOT_TOKEN='5405146881:AAEWI1C917dE_K8JzKwR4F_v2s1s8jX-8aw'
+
+    # this 3 checks (if) are not necessary but should be convenient
+
+    if [ "$1" == "-h" ]; then
+      echo "Usage: `basename $0` \"text message\""
+      exit 0
+    fi
+
+    if [ -z "$1" ]
+          then
+            echo "Add message text as second arguments"
+          exit 0
+    fi
+
+    if [ "$#" -ne 1 ]; then
+        echo "You can pass only one argument. For string with spaces put it on quotes"
+        exit 0
+    fi
+
+    curl -s --data "text=$1" --data "chat_id=$GROUP_ID" 'https://api.telegram.org/bot'$BOT_TOKEN'/sendMessage' > /dev/null
+
+    chmod +x /root/telegram.sh
+    cd
+    ./telegram.sh "Server $IP Installation Finished"
